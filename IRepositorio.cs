53f020e96be3bce8eb50400f2d055eed38ffe1d4
JaisonMachado <92221583+JaisonMachado@users.noreply.github.com>
2021-10-31 14:07:36 -0300

    Add files via upload
    Arquivos isolados

---
+++ IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Entretenimento.Interfaces
+{
+    interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Atualiza(int d, T entidade);
+        void Exclui(int id);
+        int ProximoId();
+
+    }
+}
